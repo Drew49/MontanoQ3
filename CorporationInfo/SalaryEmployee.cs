e424c3b811c8326ba0906faa178598c94b1d19de
ANDREW MONTANO <amontano59@cnm.edu>
2022-02-28 20:29:41 -0700

    Add project files.

---
+++ CorporationInfo/SalaryEmployee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    class SalaryEmployee:Employee
+    {
+        public decimal Salary { get; set; }
+
+        public SalaryEmployee()
+            : this("", "", 30000.0M)
+        {            
+        }
+
+        public SalaryEmployee(string firstName, string lastName, decimal salary, string phone = "TBD", string email = "TBD")
+            :base(firstName,lastName, phone, email)
+        {
+        }
+
+        public override string PaySummary
+        {
+            get
+            {
+                return "This employee is payed " + Salary + " per year";
+            }
+        }
+
+        public override decimal Pay(int start, int end)
+        {
+            const int  payPeriodsPerYear = 24;
+            decimal pay = Salary * (end - start) / payPeriodsPerYear;
+            return pay;
+        }
+
+        public string ToString()
+        {
+            return base.ToString() + " Salary Employee";
+        }
+    }
+}
